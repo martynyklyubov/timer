946adb4999d25b2c698e079f3e19d0ccdec072cc
Lyubov Martynyk <50379289+martynyklyubov@users.noreply.github.com>
2020-05-20 14:47:41 -0700

    added a new button

--- timer.py
+++ timer.py
@@ -4,45 +4,37 @@ import time
 
 fen = Tk()
 fen.title("COUNTDOWN")
-
 canv = Canvas(fen, height=100, width=200, bg="orange")
-canv.grid(row=0, column=0, rowspan=2, padx=5, pady=5)
+canv.grid(row=0, column=0, rowspan=3, padx=8, pady=8)
 
-def start(t=25):
-    global i
+global i
+i = 0
+
+# def restart():
+
+
+def start(t=5):
+
     i = 1
     if (i == 1):
-        lab.config(text=str(t), font=("Impact", 18))
+        lab.config(text=str(t), font=("Impact", 30))
         if (t>0):
             fen.after(1000, start, t-1)
     else:
         i = 1
         lab.config(text="stop", fg="black", bg="orange", font=("Impact", 18))
-
 lab = Label(fen, text="", fg="black", bg="orange", font=("Impact", 18))
-lab.grid(row=0, column=0)
+lab.grid(row=1, column=0)
 
-Countdown = Button(fen, text="START", command=start, fg="black", bg="orange", font=("Impact", 18))
-Countdown.grid(row=1, column=0)
+def stop(t):
+    global i
+    i = t
+
+Countdown1 = Button(fen, text="START", command=start, fg="black", bg="orange", font=("Impact", 18))
+Countdown1.grid(row=0, column=1, sticky = E)
+Countdown2 = Button(fen, text="STOP", command=stop, fg="black", bg="orange", font=("Impact", 18))
+Countdown2.grid(row=1, column=1, sticky = E)
+Countdown3 = Button(fen, text="RESET", command=stop, fg="black", bg="orange", font=("Impact", 18))
+Countdown3.grid(row=2, column=1, sticky = E)
 
 fen.mainloop()
-
-"""
-different method
-"""
-# import tkinter as tk
-# import time as tm
-
-# def display_time():
-#     current_time = tm.strftime('%H:%M:%S')
-#     clock_label['text'] = current_time
-#     root.after(1000, display_time)
-
-# root = tk.Tk()
-# root.title('Digital Clock')
-# clock_label = tk.Label(root,font='ariel 100', bg="orange", fg="black")
-# button = tk.Button(root, text='START')
-# button.pack()
-# clock_label.grid(row=0,column=0)
-# display_time()
-# root.mainloop() 
