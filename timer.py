6d3139f0b25195d36125e8482bb6752c2c39b478
Lyubov Martynyk <50379289+martynyklyubov@users.noreply.github.com>
2020-05-21 12:25:53 -0700

    right format

--- timer.py
+++ timer.py
@@ -6,18 +6,17 @@ fen = Tk()
 fen.title("COUNTDOWN")
 canv = Canvas(fen, height=100, width=200, bg="orange")
 canv.grid(row=0, column=0, rowspan=3, padx=8, pady=8)
+lab = Label(fen, text="", fg="black", bg="orange", font=("Impact", 18))
+lab.grid(row=1, column=0)
 
-global i
-i = 0
-
-# def restart():
-
-
-def start(t=5):
-
+global i 
+i = 1
+def start(t=1500):
+    mins, secs = divmod(t, 60)
+    timer ='{:02d}:{:02d}'.format(mins, secs)
     i = 1
     if (i == 1):
-        lab.config(text=str(t), font=("Impact", 30))
+        lab.config(text=timer, font=("Impact", 30))
         if (t>0):
             fen.after(1000, start, t-1)
     else:
@@ -26,9 +25,32 @@ def start(t=5):
 lab = Label(fen, text="", fg="black", bg="orange", font=("Impact", 18))
 lab.grid(row=1, column=0)
 
-def stop(t):
-    global i
-    i = t
+
+# def start(t = 5):
+#     mins, secs = divmod(t, 60)
+#     t = '{:02d}:{:02d}'.format(mins,secs)
+#     if (i == 1):
+#         lab.config(text=str(t), font=("Impact", 30))
+#         if (i > 0):
+#             fen.after(1000, mins, secs)
+
+# def start(t=15):
+    # while t:
+    #     mins, secs = divmod(t, 60)
+    #     timer ='{:02d}:{:02d}'.format(mins, secs)
+    #     lab.config(text=str(t))
+    #     time.sleep(1)
+    #     lab.config(text=str(t))
+    #     t -= 1
+    # lab.config(text=str(t))
+    # print("Time is out!")
+
+def restart():
+    exit()
+
+
+def stop():
+    exit()
 
 Countdown1 = Button(fen, text="START", command=start, fg="black", bg="orange", font=("Impact", 18))
 Countdown1.grid(row=0, column=1, sticky = E)
@@ -38,3 +60,24 @@ Countdown3 = Button(fen, text="RESET", command=stop, fg="black", bg="orange", fo
 Countdown3.grid(row=2, column=1, sticky = E)
 
 fen.mainloop()
+
+
+"""
+different method
+"""
+# import tkinter as tk
+# import time as tm
+
+# def display_time():
+#     current_time = tm.strftime('%H:%M:%S')
+#     clock_label['text'] = current_time
+#     root.after(1000, display_time)
+
+# root = tk.Tk()
+# root.title('Digital Clock')
+# clock_label = tk.Label(root,font='ariel 100', bg="orange", fg="black")
+# button = tk.Button(root, text='START')
+# button.pack()
+# clock_label.grid(row=0,column=0)
+# display_time()
+# root.mainloop() 
